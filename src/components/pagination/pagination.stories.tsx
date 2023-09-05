cf7bb7f4d073aa9cbea81b7cc052dfe9af9af91a
Tania <evdunova.tania@gmail.com>
2023-09-05 18:24:42 +0200

    add story pagination

---
+++ src/components/pagination/pagination.stories.tsx
@@ -0,0 +1,19 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { Pagination } from './'
+
+const meta = {
+  title: 'Components/Pagination',
+  component: Pagination,
+  tags: ['autodocs'],
+} satisfies Meta<typeof Pagination>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const CardsPagination: Story = {
+  args: {
+    contentPerPage: 1,
+    count: 16,
+  },
+}
