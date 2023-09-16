f39846bbd05552ce53fd0ed6763165d373d60130
Tania <evdunova.tania@gmail.com>
2023-09-16 21:35:40 +0200

    add router

---
+++ src/router.tsx
@@ -0,0 +1,45 @@
+import {
+  createBrowserRouter,
+  Navigate,
+  Outlet,
+  RouteObject,
+  RouterProvider,
+} from 'react-router-dom'
+
+import { useGetDecksQuery } from './services/base-api'
+
+const publicRoutes: RouteObject[] = [
+  {
+    path: '/login',
+    element: <div>login</div>,
+  },
+]
+
+const privateRoutes: RouteObject[] = [
+  {
+    path: '/',
+    element: <div>hello</div>,
+  },
+]
+
+const router = createBrowserRouter([
+  {
+    element: <PrivateRoutes />,
+    children: privateRoutes,
+  },
+  ...publicRoutes,
+])
+
+function PrivateRoutes() {
+  const isAuthenticated = false
+
+  return isAuthenticated ? <Outlet /> : <Navigate to="/login" />
+}
+
+export const Router = () => {
+  const result = useGetDecksQuery()
+
+  console.log(result)
+
+  return <RouterProvider router={router} />
+}
